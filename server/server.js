f2f71e7edc9a247622a31ed53f844f4ee706e056
Dhairya Nadapara <dhairyanadapara98@gmail.com>
2017-08-15 11:39:08 +0530

    mongoose script update

--- server/server.js
+++ server/server.js
@@ -5,24 +5,23 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo',{
     text:{
-        type:String
+        type:String,
+        require:true,
+        minlength:1,
+        trim:true
     },
     completed:{
-        type:Boolean
+        type:Boolean,
+        default:false
     },
     completedAt:{
-        type: Number
+        type: Number,
+        default:null
     }
 });
 
-var newTodo = new Todo({
+/*var newTodo = new Todo({
     text:'Cook dinner'
-});
-
-var otherTodo = new Todo({
-    text:'Check Emails',
-    completed:true,
-    completedAt:9
 });
 
 newTodo.save().then((doc)=>{
@@ -31,8 +30,34 @@ newTodo.save().then((doc)=>{
     console.log('unable to save todo');
 });
 
+
+var otherTodo = new Todo({
+    text:'Clean the vehicle',
+});
+
+
+
 otherTodo.save().then((doc) => {
     console.log('Saved todo',JSON.stringify(doc, undefined, 2))
+}, (err) => {
+    console.log('unable to save todo');
+});*/
+
+var User = mongoose.model('User',{
+    email:{
+        type:String,
+        require:true,
+        minlength:1,
+        trim:true
+    }
+});
+
+var user1 = new User({
+    email:'dhairyanadapara98@gmail.com'
+});
+
+user1.save().then((doc) => {
+    console.log('Saved todo', JSON.stringify(doc, undefined, 2))
 }, (err) => {
     console.log('unable to save todo');
 });
