e8632f36b080672e62cb31f2b685f9acfb0f3bcf
Dhairya Nadapara <dhairyanadapara98@gmail.com>
2017-09-16 12:30:25 +0530

    DELETE route

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
-const port =process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 var app = express();
 
 
@@ -51,9 +51,32 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
-app.listen(port, () => {
-    console.log(`Server is UP on ${port}`);
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send(todo)
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
+
+        
+    
+
+
+app.listen(port, () => {
+        console.log(`Server is UP on ${port}`);
+    });
 
 
 module.exports = {
